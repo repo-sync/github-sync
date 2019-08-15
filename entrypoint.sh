1b02700e74467279c5dc8bbac13cd77ae49b04aa
Wei He <git@weispot.com>
2019-08-15 10:27:39 -0400

    feat: validate parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 set -e
 
 if [[ -z "$GITHUB_TOKEN" ]]; then
-	echo "Set the GITHUB_TOKEN environment variable."
-	exit 1
+  echo "Set the GITHUB_TOKEN environment variable."
+  exit 1
 fi
 
 if [[ -z "$SSH_PRIVATE_KEY" ]]; then
--- github-sync.sh
+++ github-sync.sh
@@ -5,6 +5,16 @@ set -e
 UPSTREAM_REPO=$1
 BRANCH_MAPPING=$2
 
+if [[ -z "$UPSTREAM_REPO" ]]; then
+  echo "Missing \$UPSTREAM_REPO"
+  exit 1
+fi
+
+if [[ -z "$BRANCH_MAPPING" ]]; then
+  echo "Missing \$SOURCE_BRANCH:\$DESTINATION_BRANCH"
+  exit 1
+fi
+
 if ! echo $UPSTREAM_REPO | grep '\.git'
 then
   UPSTREAM_REPO="https://github.com/${UPSTREAM_REPO}.git"
