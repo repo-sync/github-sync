c80145dbcadee21d5c254b53d9c494eea7742f07
Wei He <git@weispot.com>
2019-08-15 12:06:39 -0400

    fix: ssh private key handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,17 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
   exit 1
 fi
 
-if [[ -z "$SSH_PRIVATE_KEY" ]]; then
+if [[ ! -z "$SSH_PRIVATE_KEY" ]]; then
+  echo "Saving SSH_PRIVATE_KEY"
+
   mkdir -p /root/.ssh
   echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
   chmod 600 /root/.ssh/id_rsa
-fi
 
-# Github action changes $HOME to /github at runtime
-# therefore we always copy the SSH key to $HOME (aka. ~)
-mkdir -p ~/.ssh
-cp /root/.ssh/* ~/.ssh/ 2> /dev/null || true 
+  # Github action changes $HOME to /github at runtime
+  # therefore we always copy the SSH key to $HOME (aka. ~)
+  mkdir -p ~/.ssh
+  cp /root/.ssh/* ~/.ssh/ 2> /dev/null || true 
+fi
 
 sh -c "/github-sync.sh $*"
