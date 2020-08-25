32f5a7606cf2ae8f915bacaadb50c237f0a075d1
Wei He <git@weispot.com>
2020-08-25 07:43:57 -0400

    fix: add logging

--- github-sync.sh
+++ github-sync.sh
@@ -24,10 +24,20 @@ echo "UPSTREAM_REPO=$UPSTREAM_REPO"
 echo "BRANCHES=$BRANCH_MAPPING"
 
 git config --unset-all http."https://github.com/".extraheader || :
+
+echo "Resetting origin to: https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
 git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
+
+echo "Adding tmp_upstream $UPSTREAM_REPO"
 git remote add tmp_upstream "$UPSTREAM_REPO"
+
+echo "Fetching tmp_upstream"
 git fetch tmp_upstream
 git remote --verbose
+
+echo "Pushing changings from tmp_upstream to origin"
 git push origin "refs/remotes/tmp_upstream/${BRANCH_MAPPING%%:*}:refs/heads/${BRANCH_MAPPING#*:}" --force
+
+echo "Removing tmp_upstream"
 git remote rm tmp_upstream
 git remote --verbose
